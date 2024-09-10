f926f884819e465eaf02a85b4536f2eba47c8c27
Ghada-Mohamed-Khalaf <gmo625112@gmail.com>
2024-09-10 23:50:51 +0300

    create splash screen  and convert statelessWidget to statefulWidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,43 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:groceries_app/views/home_view.dart';
 import 'package:groceries_app/views/splash_view.dart';
+import 'package:groceries_app/widgets/onboarding_body.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp (DevicePreview(
+  enabled: !kReleaseMode,
+  builder: (context) => GroceriesApp(), // Wrap your app
+  ),
+  );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class GroceriesApp extends StatelessWidget {
+  const GroceriesApp({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: SplashView(),
-    );
+    return ScreenUtilInit(
+        designSize: const Size(360, 690),
+    minTextAdapt: true,
+    splitScreenMode: true,
+    // Use builder only if you need to use library outside ScreenUtilInit context
+    builder: (_ , child) {
+      return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        useInheritedMediaQuery: true,
+        locale: DevicePreview.locale(context),
+        builder: DevicePreview.appBuilder,
+        theme: ThemeData.light(),
+        darkTheme: ThemeData.dark(),
+        home:OnboardingBody()
+      );
+    } );
+
+        }
   }
-}
 
 
--- lib/views/splash_view.dart
+++ lib/views/splash_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:groceries_app/constant.dart';
 
 class SplashView extends StatefulWidget {
@@ -14,7 +15,12 @@ class _SplashViewState extends State<SplashView> {
     return Scaffold(
 backgroundColor: kPrimerColor,
       body: Center(
+        child: SizedBox(
+        height: 300.h,
+        width: 300.w,
+
         child: Image.asset("assets/image/img_2.png"),
+      ),
       ),
     );
   }
