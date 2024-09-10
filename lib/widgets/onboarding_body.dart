6bf74f6af45423a37e9a3405c11c5fccbacd6110
Ghada-Mohamed-Khalaf <gmo625112@gmail.com>
2024-09-10 23:56:23 +0300

    create onBoarding screen  and responsive

--- lib/widgets/onboarding_body.dart
+++ lib/widgets/onboarding_body.dart
@@ -1,44 +1,44 @@
-// import 'package:flutter/material.dart';
-// import 'package:flutter_screenutil/flutter_screenutil.dart';
-//
-// class OnboardingBody extends StatelessWidget {
-//   const OnboardingBody({super.key});
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return SafeArea(
-//       child: Container(
-//         decoration: BoxDecoration(
-//           image: DecorationImage(
-//               image: AssetImage(
-//             "assets/image/img.png",
-//           )),
-//         ),
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: [
-//             Image.asset(
-//               "assets/image/img_3.png",
-//             width:200.w,
-//               height:60.h,
-//             ),
-//             Text(
-//               "Welcome",
-//               style: TextStyle(
-//                   fontSize: 30.sp,
-//                   fontWeight: FontWeight.w600,
-//                   // fontFamily: "Gilroy-Medium",
-//                   color: Colors.white),
-//             ),
-//             Text("to our store", style: TextStyle(
-//                 fontSize: 15.sp,
-//                 fontWeight: FontWeight.w600,
-//                 fontFamily: "Gilroy-Medium",
-//
-//                 color: Colors.white),),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class OnboardingBody extends StatelessWidget {
+  const OnboardingBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage(
+            "assets/image/img.png",
+          )),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              "assets/image/img_3.png",
+            width:200.w,
+              height:60.h,
+            ),
+            Text(
+              "Welcome",
+              style: TextStyle(
+                  fontSize: 30.sp,
+                  fontWeight: FontWeight.w600,
+                  // fontFamily: "Gilroy-Medium",
+                  color: Colors.white),
+            ),
+            Text("to our store", style: TextStyle(
+                fontSize: 15.sp,
+                fontWeight: FontWeight.w600,
+                fontFamily: "Gilroy-Medium",
+
+                color: Colors.white),),
+          ],
+        ),
+      ),
+    );
+  }
+}
